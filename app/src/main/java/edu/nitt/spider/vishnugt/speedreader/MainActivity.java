950e31ca46600f2f2c1217fa13bb8b1080888d76
praba1110 <praba1110@gmail.com>
2016-03-13 17:57:40 +0530

    Added option to read multiple words at a time

--- app/src/main/java/edu/nitt/spider/vishnugt/speedreader/MainActivity.java
+++ app/src/main/java/edu/nitt/spider/vishnugt/speedreader/MainActivity.java
@@ -6,20 +6,34 @@ package edu.nitt.spider.vishnugt.speedreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
+    Spinner s_speed,n_words;
     TextView outputText;
     Button btn;
     EditText speedview, contenttoread;
-
+    int iterator=0;
+    int speed = 400;
+    String[] textsplitted;
+    boolean isReading = false;
+    int no=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +42,92 @@ public class MainActivity extends AppCompatActivity {
 
         //initializing views
         outputText = (TextView)findViewById(R.id.outtext);
-        speedview = (EditText)findViewById(R.id.speed);
+      //  speedview = (EditText)findViewById(R.id.speed);
         contenttoread = (EditText)findViewById(R.id.contenttoread);
         btn = (Button)findViewById(R.id.readbtn);
+        s_speed= (Spinner) findViewById(R.id.speedspinner);
+        n_words= (Spinner) findViewById(R.id.wordsSpinner);
+
+
+        initializeValues();
+
+        s_speed.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        speed=500;
+                        break;
+                    case 1:
+                        speed=400;
+                        break;
+                    case 2:
+                        speed=250;
+                        break;
+                    case 3:
+                        speed=150;
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                speed=200;
+            }
+        });
+
+        n_words.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        no=1;
+
+                        break;
+                    case 1:
+                        no=2;
+
+                        break;
+                    case 2:
+                        no=3;
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                no=1;
+            }
+        });
+
     }
 
 
-    int iterator=0;
-    int speed = 400;
-    String textsplitted[];
-    boolean isReading = false;
+    public void initializeValues(){
+        //Initializing values for spinners
+
+
+        List<String> speedValues=new ArrayList<>();
+        speedValues.add("Slow");
+        speedValues.add("Medium");
+        speedValues.add("Fast");
+        speedValues.add("SuperFast");
+
+        List<String> words=new ArrayList<>();
+        words.add("1");
+        words.add("2");
+        words.add("3");
+
+        ArrayAdapter<String> speedAdapter=new ArrayAdapter<String>
+                (this,android.R.layout.simple_spinner_dropdown_item,speedValues);
+        s_speed.setAdapter(speedAdapter);
+
+        ArrayAdapter<String> wordAdapter=new ArrayAdapter<String>
+                (this,android.R.layout.simple_spinner_dropdown_item,words);
+        n_words.setAdapter(wordAdapter);
+    }
+
 
     public void btnfunction(View v)
     {
@@ -52,7 +142,24 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                outputText.setText(textsplitted[iterator++]);
+                                int j=0;
+                                String text="";
+                                //TODO: check if next string exists in splitted array and only then add it to text
+                                while ( j<no){
+                                        if(textsplitted[iterator+1]!=null) {
+                                            text += textsplitted[iterator++];
+                                            text += " ";
+
+                                            j++;
+                                        }
+                                    else
+                                            break;
+
+
+
+                                }
+                                Log.d("text",text);
+                                outputText.setText(text);
                                 if(iterator==textsplitted.length-2)
                                 {
                                     isReading=false;
@@ -82,14 +189,14 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Content to be read is Empty :(", Toast.LENGTH_SHORT).show();
             return;
         }
-
+        /*
         speed = Integer.parseInt(speedview.getText().toString());
         if(speedview.getText().toString().isEmpty() || speed==0)
         {
             Toast.makeText(this, "Speed can't be null, setting it to 400!", Toast.LENGTH_SHORT).show();
             speed=400;
         }
-
+        */
         isReading = true;
         iterator=0;
         textsplitted = contenttoread.getText().toString().split(" ");
